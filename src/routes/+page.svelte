2f1edca19f4095f8b6c980dfdaae80db4502584f
fsb3rke <berkeeavcii@gmail.com>
2024-01-25 01:38:54 +0300

    feat: integrate back-end to svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Lookup from "./lookup.svelte";
+</script>
+
+<div class="container">
+    <Lookup />
+</div>
---
+++ src/routes/lookup.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+
+    let username: string;
+    let globalName: string;
+    let avatarLink: string;
+    let bannerLink: string;
+
+    const showData = async () => {
+        const inputDataValue: string = (<HTMLInputElement>document.getElementById("inp_id")).value;
+        
+
+        try {
+            console.log(`http://localhost:2117/api/dclookup?id=${inputDataValue}`);
+            
+            const response = await fetch(`http://localhost:2117/api/dclookup?id=${inputDataValue}`);
+            const data = await response.json()
+            console.log(data);
+
+            username = data["username"];
+            globalName = data["globalName"];
+            avatarLink = data["avatarLink"];
+            bannerLink = data["bannerLink"];
+
+
+        } catch (err) {
+            console.error("Fetch Error:", err);
+        }
+        
+    }
+
+
+</script>
+
+<div>
+    <span>Account ID: </span>
+    <input type="text" id="inp_id">
+
+    <button id="btn_showData" on:click={() => showData()}>
+        Show The Data
+    </button>
+
+    <div>{username}</div>
+    <div>{globalName}</div>
+    <img src="{avatarLink}" alt="">
+    <img src="{bannerLink}" alt="">
+
+</div>
