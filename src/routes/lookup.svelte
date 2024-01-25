a3076cf7ef75c373d323f38bf3a3912605122952
fsb3rke <berkeeavcii@gmail.com>
2024-01-25 20:16:14 +0300

    refactor: length of idv

--- src/routes/lookup.svelte
+++ src/routes/lookup.svelte
@@ -11,28 +11,28 @@
     const showData = async () => {
         const inputDataValue: string = (<HTMLInputElement>document.getElementById("inp_id")).value;
         
-        if (inputDataValue.length >= 17 && inputDataValue.length <= 18) {
-            try {
-                console.log(`http://localhost:2117/api/dclookup?id=${inputDataValue}`);
-                
-                const response = await fetch(`http://localhost:2117/api/dclookup?id=${inputDataValue}`);
-                const data = await response.json()
-                console.log(data);
-
-                username = data["username"];
-                globalName = data["globalName"];
-                avatarURL = data["avatarURL"];
-                bannerURL = data["bannerURL"];
-                hexAccentColor = data["hexAccentColor"];
-                createdAt = data["createdAt"];
-                isBot = data["isBot"];
-
-
-            } catch (err) {
-                console.error("Fetch Error:", err);
-            }
+        if (inputDataValue.length < 17) {
+            return;
         }
 
+        try {
+            console.log(`http://localhost:2117/api/dclookup?id=${inputDataValue}`);
+            
+            const response = await fetch(`http://localhost:2117/api/dclookup?id=${inputDataValue}`);
+            const data = await response.json()
+
+            console.log(data);
+            
+            username = data["username"];
+            globalName = data["globalName"];
+            avatarURL = data["avatarURL"];
+            bannerURL = data["bannerURL"];
+            hexAccentColor = data["hexAccentColor"];
+            createdAt = data["createdAt"];
+            isBot = data["isBot"];
+        } catch (err) {
+            console.error("Fetch Error:", err);
+        }
         
     }
 
